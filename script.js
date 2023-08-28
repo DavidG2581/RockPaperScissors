3e95aebe3ea3b88f5eedcd85e145ab0d6f9efc26
David <davidgobar@rocketmail.com>
2023-08-28 13:28:12 -0400

    testing Math functions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let choices = ['rock','paper','scissors'];
 
 function computerChoice(choices){
     let choice = Math.floor(choices.length * Math.random());
+    console.log(Math.random())
+    console.log('choice number is ',choice)
     console.log('Computer choice is',choices[choice]);
 }
 
