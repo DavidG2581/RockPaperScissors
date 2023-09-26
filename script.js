c3510220d7fa98e67f8bd544a33167c1318663c7
David <davidgobar@rocketmail.com>
2023-09-26 19:03:36 -0400

    Game works with buttons now

--- script.js
+++ script.js
@@ -4,27 +4,19 @@ let choices = ['rock','paper','scissors'];
 
 
 //UI stuff
+const btns = document.querySelectorAll('.game-btn');
 
-
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('.game-button');
-
-// we use the .forEach method to iterate through each button
-
-buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    console.log(button.id);
-  });
+btns.forEach((btn) => {btn.addEventListener('click',() => {
+    //console.log(btn.textContent);
+    game(btn);
+})
 });
 
-// const buttons = document.querySelectorAll('button');
-// buttons.forEach((button) => {button.addEventListener("click", ()=>alert('button clicked') )});
+//**** VERY IMPORTANT SYNTAX: both forEach() and addEventListener require a callback function or arrow function
+//arrayName.forEach(callbackfunction(item))  
+//arrayName.forEach((item) => [item.addEventListener('action',callBackFunction/arrow function)]) */
 
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //game functions
 
 function computerChoice(choices){
@@ -37,36 +29,30 @@ function computerChoice(choices){
     
 }
 
-function playerChoice(e){
-    console.log(e);
-
-    // let x=0;
-    // while (x==0){
-    //     let playerSelection = prompt("1 = Paper, 2 = Scissors, 3 = Rock");
-    //     if(playerSelection>=1&&playerSelection<=3){
-    //         //console.log('Player choice is',choices[playerSelection-1])
-    //         return choices[playerSelection-1];
-    //     }else{
-    //         alert('Number must be 1, 2, or 3')
-    //     } ;
-    // }    
-    
+function gameOver(){
+    alert('GAME OVER')
+    console
+    btns.forEach((btn) => {btn.disabled = true;});
+    const options = document.querySelector('#options');
 }
 
-function game(){
-    let rounds = 20;
+let i = 0;
+function game(playersChoice){
+
+    let rounds = 5;
     let pWins = 0;
     let cWins = 0;
-    for (i = 1; i<=rounds; i++){
-        console.log('ROUND',i)
+    
+    if (i<rounds){
+        console.log('ROUND',i+1)
         let comp = computerChoice(choices);
-        let player = playerChoice();
+        let player = playersChoice.id;
         console.log('Computer:',comp,'vs Player:',player);
         if (player == comp){
             console.log('It\'s a tie!')
-            continue
+            //continue
         }
-        if (player=='rock'){
+        else if (player=='rock'){
             if (comp=='paper'){
                 cWins++;
                 console.log('Computer wins');
@@ -96,25 +82,40 @@ function game(){
                 pWins++;
                 console.log('Player wins');
             }
-
         }else {
-            console.log('ERROR')
-            alert('ERROR');
+            console.log('ERROR');
         }
-
-
+        i++
     }
-    if (pWins>cWins){
-        console.log('Winner is player!!!');
-    }else if (pWins<cWins){
-        console.log('Winner is computer!!!');
-    }else if (pWins==cWins){
-        console.log('It\s a tie. Nobody wins...');
-    }else{
-        console.log('There is a disturbance in the force');
+    if(i == rounds){
+        if (pWins>cWins){
+            console.log('Winner is player!!!');
+        }else if (pWins<cWins){
+            console.log('Winner is computer!!!');
+        }else if (pWins==cWins){
+            console.log('It\s a tie. Nobody wins...');
+        }else{
+            console.log('There is a disturbance in the force');
+        }
+        gameOver();
+        
     }
 }
 
-//game();
+//game(playersChoice);
 
+// function playerChoice(e){
+    
 
+//     // let x=0;
+//     // while (x==0){
+//     //     let playerSelection = prompt("1 = Paper, 2 = Scissors, 3 = Rock");
+//     //     if(playerSelection>=1&&playerSelection<=3){
+//     //         //console.log('Player choice is',choices[playerSelection-1])
+//     //         return choices[playerSelection-1];
+//     //     }else{
+//     //         alert('Number must be 1, 2, or 3')
+//     //     } ;
+//     // }    
+    
+// }
